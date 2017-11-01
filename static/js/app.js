70b38f829e2dd0c96ad1503f753c83f27be47191
KevinMind <kmeinhardt7@gmail.com>
2017-11-01 09:51:55 -0500

    update ui

--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,10 @@ $(document).ready(function() {
   console.log("done collapisifying.");
 
 
-$(".list-item").click(function(event) {
-
+$(".collapsible-header").click(function(event) {
+  if($(this).parent().hasClass("active")) {
+    console.log("is active..");
+  } else {
     let id = $(this).data("scroll");
     let destination = document.getElementById(id);
     let height = $("#item-list-header").height();
@@ -17,10 +19,8 @@ $(".list-item").click(function(event) {
       $('html, body').animate({
           scrollTop: ($(destination).position().top - 50)
       }, 300);
-      console.log(height);
-      console.log(destination)
-    }, 300, [destination, height]);
-
+    }, 600, [destination, height]);
+  }
 
 });
 
